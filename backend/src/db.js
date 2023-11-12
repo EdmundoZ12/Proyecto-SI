4087f309e8e8a95cbce50ab4eb14108af82f7e25
EdmundoZ12 <edmundozentenor12@gmail.com>
2023-11-12 17:03:56 -0400

    CAMBIO

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,12 +1,16 @@
 const {Pool} = require('pg');
 const {db} = require('./config');
+const { config } = require("dotenv");
+config()
 
 const pool=new Pool({
-    user: db.user,
+    connectionString:process.env.DATABASE_URL,
+    ssl:true
+    /*user: db.user,
     password: db.password,
     host: db.host,
     port: db.port,
-    database: db.database
+    database: db.database*/
 });
 
 module.exports=pool;
