47133a7a75411fa88ab307147c7ee06c02748bd7
EdmundoZ12 <edmundozentenor12@gmail.com>
2023-11-13 02:04:24 -0400

    ss

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,7 +3,7 @@ const {db} = require('./config');
 
 const pool=new Pool({
     connectionString:'postgres://edmundo:O9TaecdxXFPRfGxjTB40ME4h9IqqoI99@dpg-cl8jmjn6e7vc73a7449g-a.oregon-postgres.render.com/dbgimmasio',
-    //ssl:true
+    ssl:true
 });
 
 module.exports=pool;
